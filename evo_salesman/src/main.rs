6fdfc6040d48bc9f74188786b432d33a20758a85
missingno7 <missingno@seznam.cz>
2021-12-28 19:55:19 +0100

    Updates

--- evo_salesman/src/main.rs
+++ evo_salesman/src/main.rs
@@ -4,37 +4,37 @@ extern crate evo_salesman;
 use revo::pop_config::PopulationConfig;
 use revo::population::Population;
 use revo::evo_individual::EvoIndividual;
-use evo_salesman::salesman::{SalesmanIndividualData, SalesmanIndividual};
+use evo_salesman::salesman::{SalesmanIndividualData, SalesmanIndividual, SalesmanInitType};
 
 fn main() {
-
-    let n_cities: u32 = 100;
-    let screen_width: u32 = 500;
-    let screen_height: u32 = 500;
+    let n_cities: u32 = 500;
+    let screen_width: u32 = 1000;
+    let screen_height: u32 = 1000;
     let shift_prob: f64 = 0.4;
     let rev_prob: f64 = 0.4;
 
     let mut rng = rand::thread_rng();
 
     let pop_config = PopulationConfig::new("pop_config.json");
-    let ind_data = SalesmanIndividualData::new(&mut rng, n_cities, screen_width, screen_height,shift_prob,rev_prob);
+    let ind_data = SalesmanIndividualData::new(&mut rng, n_cities, screen_width, screen_height, shift_prob, rev_prob, SalesmanInitType::GreedyJoining);
     let mut pop: Population<SalesmanIndividual, SalesmanIndividualData> = Population::new(pop_config, ind_data.clone());
 
-
     let mut all_best_ind = pop.get_best();
-    for _ in 0..500000
+
+    all_best_ind.draw(format!("best_{}.png", pop.get_generation()).as_str(), &ind_data);
+    println!("Round {}, best fitness: {}", pop.get_generation(), all_best_ind.get_fitness());
+
+    loop
     {
         let best_ind = pop.get_best();
+
         if best_ind.get_fitness() > all_best_ind.get_fitness()
         {
+            println!("Round {}, best fitness: {}", pop.get_generation(), best_ind.get_fitness());
             all_best_ind = best_ind.clone();
-            all_best_ind.draw(format!("best_{}.png",pop.get_generation()).as_str(), &ind_data);
-            println!("Round {}, best fitness: {}",pop.get_generation(), best_ind.get_fitness());
+            all_best_ind.draw(format!("best_{}.png", pop.get_generation()).as_str(), &ind_data);
         }
 
         pop.next_gen();
-
     }
-
-
 }
--- evo_salesman/src/salesman.rs
+++ evo_salesman/src/salesman.rs
@@ -1,10 +1,20 @@
 use rand::Rng;
-use rand::seq::SliceRandom;
 use rand::prelude::ThreadRng;
 use revo::evo_individual::EvoIndividual;
 use image::{RgbImage, ImageBuffer, Rgb};
 use imageproc::drawing::{draw_hollow_rect_mut, draw_line_segment_mut};
 use imageproc::rect::Rect;
+use rand::seq::SliceRandom;
+use std::collections::HashSet;
+
+#[derive(Clone)]
+pub enum SalesmanInitType
+{
+    Naive ,
+    Noise ,
+    Insertion ,
+    GreedyJoining ,
+}
 
 pub struct SalesmanIndividualData
 {
@@ -13,13 +23,13 @@ pub struct SalesmanIndividualData
     screen_height: u32,
     shift_prob: f64,
     rev_prob: f64,
-
+    init_type: SalesmanInitType
 }
 
 
 impl SalesmanIndividualData
 {
-    pub fn new(rng: &mut ThreadRng, n_cities: u32, screen_width: u32, screen_height: u32, shift_prob: f64, rev_prob: f64) -> Self {
+    pub fn new(rng: &mut ThreadRng, n_cities: u32, screen_width: u32, screen_height: u32, shift_prob: f64, rev_prob: f64, init_type: SalesmanInitType) -> Self {
         let mut coords: Vec<(u32, u32)> = Vec::new();
 
         for _ in 0..n_cities
@@ -37,6 +47,7 @@ impl SalesmanIndividualData
             screen_height,
             shift_prob,
             rev_prob,
+            init_type,
         }
     }
 
@@ -49,12 +60,13 @@ impl SalesmanIndividualData
             screen_height: self.screen_height,
             shift_prob: self.shift_prob,
             rev_prob: self.rev_prob,
+            init_type: self.init_type.clone(),
         }
     }
 }
 
 pub struct SalesmanIndividual {
-    fitness: f64,
+    pub fitness: f64,
     genom: Vec<u16>,
 }
 
@@ -67,17 +79,16 @@ impl SalesmanIndividual
         // Draw cities
         for i in 0..ind_data.coords.len()
         {
-            let col = ((i * 255) / (ind_data.coords.len())) as u8;
-            let city_color = Rgb([col, 255 - col, 0]);
+            let city_color = Rgb([255, 0, 0]);
 
-            let x = ind_data.coords[self.genom[i] as usize].0 as f32;
-            let y = ind_data.coords[self.genom[i] as usize].1 as f32;
+            let x = ind_data.coords[i].0 as f32;
+            let y = ind_data.coords[i].1 as f32;
             draw_hollow_rect_mut(&mut img, Rect::at(x as i32 - 5, y as i32 - 5).of_size(10, 10), city_color.clone());
         }
 
         // Draw roads
         for i in 0..self.genom.len() - 1 {
-            let col = ((i * 255) / (ind_data.coords.len())) as u8;
+            let col = ((i * 255) / (self.genom.len())) as u8;
             let road_color = Rgb([col, 255 - col, 0]);
 
             let from_x = ind_data.coords[self.genom[i] as usize].0 as f32;
@@ -204,6 +215,207 @@ impl SalesmanIndividual
             }
         }
     }
+
+    fn new_random_naive(ind_data: &SalesmanIndividualData, rng: &mut ThreadRng) -> Self
+    {
+        let mut visited: Vec<bool> = vec![false; ind_data.coords.len()];
+        let mut genom: Vec<u16> = (0 as u16..ind_data.coords.len() as u16).collect();
+        let starting_city = rng.gen_range(0..ind_data.coords.len() - 1);
+
+        genom[0] = starting_city as u16;
+        visited[starting_city] = true;
+
+        for i in 1..genom.len()
+        {
+            let mut closest_dist = std::f64::MAX;
+            let mut closest_j = 0;
+
+            for j in 0..ind_data.coords.len()
+            {
+                if i == j || visited[j] { continue; }
+
+                let x1 = ind_data.coords[genom[i - 1] as usize].0;
+                let y1 = ind_data.coords[genom[i - 1] as usize].1;
+                let x2 = ind_data.coords[j].0;
+                let y2 = ind_data.coords[j].1;
+                let distance = SalesmanIndividual::distance(x1, y1, x2, y2);
+
+                if distance < closest_dist
+                {
+                    closest_j = j;
+                    closest_dist = distance;
+                }
+            }
+            visited[closest_j] = true;
+            genom[i] = closest_j as u16;
+        }
+
+        SalesmanIndividual
+        {
+            genom,
+            fitness: 0.0,
+        }
+    }
+
+    fn new_random_noise(ind_data: &SalesmanIndividualData, rng: &mut ThreadRng) -> Self
+    {
+        let mut genom: Vec<u16> = (0 as u16..ind_data.coords.len() as u16).collect();
+        genom.shuffle(rng);
+
+        SalesmanIndividual
+        {
+            genom,
+            fitness: 0.0,
+        }
+    }
+
+    fn new_random_insertion(ind_data: &SalesmanIndividualData, rng: &mut ThreadRng) -> Self
+    {
+        let mut cities: HashSet<u16> = (0 as u16..ind_data.coords.len() as u16).collect();
+        let mut genom: Vec<u16> = Vec::new();
+
+        for _ in 0..3
+        {
+            // Select random city
+            let available_cities: Vec<u16> = cities.clone().into_iter().collect();
+            let selected_city = if available_cities.len() > 1 { available_cities[rng.gen_range(0..available_cities.len() - 1)] } else { available_cities[0] };
+            cities.remove(&selected_city);
+
+            // Insert random city to genom
+            genom.push(selected_city);
+        }
+
+        for _ in 3..ind_data.coords.len()
+        {
+            // Select random city
+            let available_cities: Vec<u16> = cities.clone().into_iter().collect();
+            let selected_city = if available_cities.len() > 1 { available_cities[rng.gen_range(0..available_cities.len() - 1)] } else { available_cities[0] };
+            cities.remove(&selected_city);
+
+
+            let x1 = ind_data.coords[genom[0] as usize].0;
+            let y1 = ind_data.coords[genom[0] as usize].1;
+
+            let x2 = ind_data.coords[selected_city as usize].0;
+            let y2 = ind_data.coords[selected_city as usize].1;
+
+            let x3 = ind_data.coords[genom[genom.len() - 1] as usize].0;
+            let y3 = ind_data.coords[genom[genom.len() - 1] as usize].1;
+
+            let mut shortest_dist = Self::distance(x1, y1, x2, y2) + Self::distance(x2, y2, x3, y3);
+            let mut shortest_j = genom.len();
+
+
+            for j in 0..genom.len() - 1
+            {
+                let x1 = ind_data.coords[genom[j] as usize].0;
+                let y1 = ind_data.coords[genom[j] as usize].1;
+
+                let x2 = ind_data.coords[selected_city as usize].0;
+                let y2 = ind_data.coords[selected_city as usize].1;
+
+                let x3 = ind_data.coords[genom[j + 1] as usize].0;
+                let y3 = ind_data.coords[genom[j + 1] as usize].1;
+                let distance = Self::distance(x1, y1, x2, y2) + Self::distance(x2, y2, x3, y3);
+
+                if distance < shortest_dist
+                {
+                    shortest_dist = distance;
+                    shortest_j = j + 1;
+                }
+            }
+
+            // Insert random city to genom
+            genom.insert(shortest_j, selected_city);
+        }
+
+        SalesmanIndividual
+        {
+            genom,
+            fitness: 0.0,
+        }
+    }
+
+
+    fn new_random_greedy_joining(ind_data: &SalesmanIndividualData, rng: &mut ThreadRng) -> Self
+    {
+        let mut paths: Vec<Vec<u16>> = Vec::new();
+
+        for i in 0..ind_data.coords.len()
+        {
+            paths.push(vec![i as u16]);
+        }
+
+        loop
+        {
+            let selected_path = rng.gen_range(0..paths.len());
+
+            let mut insert_to_shortest: bool = false;
+            let mut shortest: usize = 0;
+            let mut shortest_distance: f64 = std::f64::MAX;
+
+
+            let (x_first, y_first) = ind_data.coords[paths[selected_path][0] as usize];
+            let (x_last, y_last) = ind_data.coords[paths[selected_path][paths[selected_path].len() - 1] as usize];
+
+
+            for i in 0..paths.len()
+            {
+                if i == selected_path { continue; }
+
+                // Insert to  shortest
+                {
+                    let (xi, yi) = ind_data.coords[paths[i][paths[i].len() - 1] as usize];
+                    let distance = SalesmanIndividual::distance(x_first, y_first, xi, yi);
+
+                    if distance < shortest_distance
+                    {
+                        shortest_distance = distance;
+                        shortest = i;
+                        insert_to_shortest = true;
+                    }
+                }
+
+                // Insert from shortest
+                {
+                    let (xi, yi) = ind_data.coords[paths[i][0] as usize];
+                    let distance = SalesmanIndividual::distance(x_last, y_last, xi, yi);
+
+                    if distance < shortest_distance
+                    {
+                        shortest_distance = distance;
+                        shortest = i;
+                        insert_to_shortest = false;
+                    }
+                }
+            }
+
+
+            if insert_to_shortest
+            {
+                // Insert selected path to shortest
+                let mut tmp_path = paths[selected_path].clone();
+                paths[shortest].append(&mut tmp_path);
+                paths.remove(selected_path);
+            } else {
+                let mut tmp_path = paths[shortest].clone();
+                paths[selected_path].append(&mut tmp_path);
+                paths.remove(shortest);
+            }
+
+
+            if paths.len() == 1
+            {
+                break;
+            }
+        }
+
+        SalesmanIndividual
+        {
+            genom: paths[0].clone(),
+            fitness: 0.0,
+        }
+    }
 }
 
 impl EvoIndividual<SalesmanIndividualData> for SalesmanIndividual {
@@ -215,14 +427,15 @@ impl EvoIndividual<SalesmanIndividualData> for SalesmanIndividual {
         }
     }
 
-    fn new_randomised(ind_data: &SalesmanIndividualData, rng: &mut ThreadRng) -> Self {
-        let mut genom: Vec<u16> = (0 as u16..ind_data.coords.len() as u16).collect();
-        genom.shuffle(rng);
 
-        SalesmanIndividual
+    fn new_randomised(ind_data: &SalesmanIndividualData, rng: &mut ThreadRng) -> Self
+    {
+        match ind_data.init_type
         {
-            genom,
-            fitness: 0.0,
+            SalesmanInitType::Naive => {Self::new_random_naive(ind_data, rng)},
+            SalesmanInitType::Noise => {Self::new_random_noise(ind_data, rng)},
+            SalesmanInitType::Insertion => {Self::new_random_insertion(ind_data, rng)},
+            SalesmanInitType::GreedyJoining => {Self::new_random_greedy_joining(ind_data, rng)},
         }
     }
 
@@ -242,8 +455,10 @@ impl EvoIndividual<SalesmanIndividualData> for SalesmanIndividual {
         }
     }
 
-    fn mutate(&mut self, ind_data: &SalesmanIndividualData, rng: &mut ThreadRng, mut_prob: f32, _mut_amount: f32)
+    fn mutate(&mut self, ind_data: &SalesmanIndividualData, rng: &mut ThreadRng, _mut_prob: f32, _mut_amount: f32)
     {
+        /*
+        Too much of unnecessary overhead
         for i in 0..self.genom.len() {
             if rng.gen_range(0.0..1.0) < mut_prob {
                 let swap_with = rng.gen_range(0..self.genom.len() - 1);
@@ -256,6 +471,7 @@ impl EvoIndividual<SalesmanIndividualData> for SalesmanIndividual {
                 self.genom[swap_with] = tmp;
             }
         }
+        */
 
         if rng.gen_range(0.0..1.0) < ind_data.shift_prob {
             // Shifting
@@ -273,16 +489,16 @@ impl EvoIndividual<SalesmanIndividualData> for SalesmanIndividual {
 
     fn crossover_to(&self, another_ind: &SalesmanIndividual, dest_int: &mut SalesmanIndividual, _ind_data: &SalesmanIndividualData, rng: &mut ThreadRng)
     {
-        let mut myarray: Vec<bool> = vec![false; self.genom.len()];
+        let mut used: Vec<bool> = vec![false; self.genom.len()];
         let cross_point = rng.gen_range(0..self.genom.len() - 1);
         let mut i = cross_point;
 
         loop {
-            if myarray[self.genom[i] as usize] {
-                if myarray[another_ind.genom[i] as usize] {
+            if used[self.genom[i] as usize] {
+                if used[another_ind.genom[i] as usize] {
                     // Both on list
                     for j in 0..self.genom.len() {
-                        if !myarray[j] {
+                        if !used[j] {
                             dest_int.genom[i] = j as u16;
                             break;
                         }
@@ -292,7 +508,7 @@ impl EvoIndividual<SalesmanIndividualData> for SalesmanIndividual {
                     dest_int.genom[i] = another_ind.genom[i];
                 }
             } else {
-                if myarray[another_ind.genom[i] as usize] {
+                if used[another_ind.genom[i] as usize] {
                     // first not used
                     dest_int.genom[i] = self.genom[i];
                 } else {
@@ -305,7 +521,7 @@ impl EvoIndividual<SalesmanIndividualData> for SalesmanIndividual {
                 }
             }
 
-            myarray[dest_int.genom[i] as usize] = true;
+            used[dest_int.genom[i] as usize] = true;
 
             i = (i + 1) % self.genom.len();
 
