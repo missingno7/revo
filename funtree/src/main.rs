62349b39a4b280f82d92a055eb241353d38d38cc
Jiri Vestfal <jiri.vestfal@fetch.ai>
2023-04-20 17:42:31 +0200

    GNUplot

--- funtree/src/main.rs
+++ funtree/src/main.rs
@@ -5,6 +5,30 @@ use revo::config::Config;
 use revo::evo_individual::EvoIndividual;
 use revo::population::Population;
 use std::fs;
+/*
+use gnuplot::{Caption, Color, Figure};
+
+fn main() {
+    // Create some example data
+    let x = vec![0.0, 1.0, 2.0, 3.0, 4.0];
+    let y = vec![0.0, 0.5, 1.0, 1.5, 2.0];
+    let y_pred = vec![0.1, 0.4, 0.9, 1.6, 1.8];
+
+    // Create a new gnuplot figure
+    let mut fg = Figure::new();
+
+    // Plot the actual values as red circles
+    fg.axes2d()
+        .points(&x, &y, &[Caption("Actual"), Color("red")]);
+
+    // Plot the predicted values as blue lines
+    fg.axes2d()
+        .lines(&x, &y_pred, &[Caption("Predicted"), Color("blue")]);
+
+    // Show the plot
+    fg.show().unwrap();
+}
+*/
 
 fn main() {
     // Prepare output directory and remove old files if they exist
